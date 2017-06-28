a0ed015e7f07d3f9b5a0c1dbf49ca2161c32e3f5
shekharbhardwaj <shekhar.nik@gmail.com>
2017-06-28 19:36:10 -0400

    Removing the loading json function and passinng the data directly After conversion.

--- src/file_util.py
+++ src/file_util.py
@@ -2,41 +2,53 @@ import csv
 import json
 import logging
 import sys
+import random
 
-def csv_to_json (csv_file_name_str, json_file_name_str):
-    try:
-        logging.info("converting {} to {}".format(csv_file_name_str, json_file_name_str))
-        csv_file = open(csv_file_name_str, "r")
-        json_file = open(json_file_name_str, "w")
-        
-        field_names = ["Name", "Height", "SoccerExperience", "GuardianName" ]
+
+# 
+# def csv_to_json (csv_file_name_str):
+#     try:
+#         logging.info("converting {} ".format(csv_file_name_str))
+#         csv_file = open(csv_file_name_str, "r")
+#         #json_file = open(json_file_name_str, "w")
+#         
+#         field_names = ["Name", "Height", "SoccerExperience", "GuardianName", "JerseyNumber"]
+#         reader = csv.DictReader(csv_file, field_names)
+#         logging.info("Conversion finish.")
+#         out = '{\n "Players": [\n\t'+ ',\n\t'.join([json.dumps(row) for row in reader]) + '\n]\n}'
+#         #json_file.write(out)
+#         return out
+#     except (OSError, IOError, TypeError) as e:
+#         logging.error("csv to json conversion failed: Caused by -> {}".format(e))
+#         
+PLAYERS_JSON_FILE_NAME = "soccre_players.json"
+
+def generate_jersey_number():
+    return "".join(str(random.sample(range(1, 99), 2)))
+
+def csv_to_json(csv_file_name_str):
+    field_names = ["Name", "Height", "SoccerExperience",
+                   "GuardianName", "JerseyNumber"]
+    # Read the rows of your CSV as dictionaries
+    with open(csv_file_name_str, "r") as csv_file:
         reader = csv.DictReader(csv_file, field_names)
-        logging.info("Conversion finish.")
-        out = '{\n "Players": [\n\t'+ ',\n\t'.join([json.dumps(row) for row in reader]) + '\n]\n}'
-        json_file.write(out)
-        return json_file_name_str
-    except (OSError, IOError, TypeError) as e:
-        logging.error("csv to json conversion failed: Caused by -> {}".format(e))
+        data = {
+            'Players': [player for player in reader]
+        }
+    # Loop over your players, assigning them a jersey number
+    for player in data['Players']:
+        player['JerseyNumber'] = generate_jersey_number()
+    # Dump the entire data structure to JSON
+    with open(PLAYERS_JSON_FILE_NAME, "w") as json_file:
+        json.dump(data, json_file)
+    return data
         
-
-def load_json(json_file_name_str):
-    
-    try:
-        with open(json_file_name_str) as json_file:
-            data = json.load(json_file)
-            return data
-    except (TypeError) as e:
-        logging.error("Json file loading failed: Caused by -> {}".format(e))
-
-
-
 
 if __name__ == "__main__":
     try:
         logging.debug("Running module from main")
-        csv_to_json (sys.argv[1], sys.argv[2])
-        data = load_json(sys.argv[2])
-        print(data["Players"][1]["Name"])
+        print(csv_to_json (sys.argv[1])["Players"][2]["Name"])
+        
     except (TypeError) as e:
         logging.error("Json file loading failed: Caused by -> {}".format(e))
     
