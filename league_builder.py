09590a7aaf6cc342bf0bd2e4c6d4513841174477
shekharbhardwaj <shekhar.nik@gmail.com>
2017-06-30 09:04:29 -0400

    minor update

--- league_builder.py
+++ league_builder.py
@@ -5,7 +5,7 @@ import sys
 # Function to be called when this module is imported, pass csv name
 #  file as an argument.
 def generate_teams(csv_file):
-    players_util.gen_digest(csv_file)
+    players_util.main(csv_file)
 
 
 if __name__ == "__main__":
